8a799749c82017ff1f081897ccbc144394fc1e51
Pratik_Tulkane <pratiktulkane12345@gmail.com>
2022-01-17 21:46:40 +0530

    Completed sign in form UI

--- src/Components/SignInForm/SignInForm.jsx
+++ src/Components/SignInForm/SignInForm.jsx
@@ -15,8 +15,8 @@ function SignInForm() {
     return (
         <div>
             <div className="container-sign-in-form">
-                <h2>Welcome back !</h2>
-                <h3>Sign in to continue to JYT</h3>
+                <h3>Welcome back !</h3>
+                <h4>Sign in to continue to JYT</h4>
                 <div className="signInSection">
                     <h5>Username</h5>
                     <input type="text" name="username" placeholder='Enter the username' />
@@ -24,12 +24,13 @@ function SignInForm() {
                     <h5>Password</h5>
                     <input type="password" name="password" placeholder='Enter Password' />
                     <button>Log in</button>
-                    <a href="">Sign in with</a>
+                    <a href="" id='signInWith'>Sign in with</a>
                     <div className="signInWith-option">
                     <img src={facebook}alt="facebook" />
                     <img src={google} alt="google" />
                     </div>
-                    <p>Don't have an account ? <a href="" id='register'>Register</a></p>
+                    <p>Don't have an account ? Register</p>
+                    <a href="" id='register'></a>
                     
                 </div>
             </div>
